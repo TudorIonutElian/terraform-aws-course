188b5068ae03cb482746ef6c3a0ed3751e044aed
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-08-28 00:01:48 +0300

    Add move-repo-to-ubuntu and update.sh scripts

---
+++ move-repo-to-ubuntu.sh
@@ -0,0 +1,3 @@
+command='ssh -i id_rsa root@45.76.95.196 ./update.sh'
+#command='ssh -i id_rsa root@45.76.95.196'
+eval $command;
---
+++ update.sh
@@ -0,0 +1,17 @@
+echo "===============================================";
+echo "=== MOVING to terraform-aws-course directory ==";
+echo "===============================================";
+
+cd terraform-aws-course;
+sleep 2;
+
+git checkout main;
+
+echo "===============================================";
+echo "========== SWITCHING to main branch ===========";
+echo "===============================================";
+
+
+git pull;
+
+
