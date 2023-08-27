c0a4f5e53fb2f2fc5c2a20e58bfe4985477dba2e
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-08-28 00:05:28 +0300

    change update.sh script

--- update.sh
+++ update.sh
@@ -1,17 +1,19 @@
-echo "===============================================";
-echo "=== MOVING to terraform-aws-course directory ==";
-echo "===============================================";
+echo -e "===============================================";
+echo -e "=== MOVING to terraform-aws-course directory ==";
+echo -e "===============================================";
 
 cd terraform-aws-course;
 sleep 2;
 
 git checkout main;
 
-echo "===============================================";
-echo "========== SWITCHING to main branch ===========";
-echo "===============================================";
+echo -e "===============================================";
+echo -e "========== SWITCHING to main branch ===========";
+echo -e "===============================================";
 
 
 git pull;
+
+eval "terraform --version";
 
 
