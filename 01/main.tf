c34f935db0c186f843a1efde159be5314bf1d8d9
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-08-28 19:48:28 +0300

    Adding first resource VPC

--- 01/main.tf
+++ 01/main.tf
@@ -13,3 +13,8 @@ provider "aws"{
   access_key = "AKIA5PWZ742ZGWPSY3NN"
   secret_key = "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"
 }
+
+# Create our first resources AWS VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
