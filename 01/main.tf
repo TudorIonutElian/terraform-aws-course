82058a8a2680a93b231d9b9950df5580b0a73788
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-08-28 19:13:56 +0300

    Bad practice --added secret access key here

--- 01/main.tf
+++ 01/main.tf
@@ -10,4 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws"{
   region = "eu-central-1"
+  access_key = "AKIA5PWZ742ZGWPSY3NN"
+  secret_key = "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"
 }
