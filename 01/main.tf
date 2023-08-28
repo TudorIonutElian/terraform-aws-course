66f539f632c128cbe6e4dc8e20c3550bffeb30b5
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-08-28 22:49:24 +0300

    Bad practice again -- add another key

--- 01/main.tf
+++ 01/main.tf
@@ -10,8 +10,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws"{
   region = "eu-central-1"
-  access_key = "AKIA5PWZ742ZGWPSY3NN"
-  secret_key = "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"
+  access_key = "AKIA5PWZ742ZP5U2MVGL"
+  secret_key = "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"
 }
 
 # Create our first resources AWS VPC
