25d2d314ce00bb7027447a4c27855d22925d70ac
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-08-28 23:07:51 +0300

    Add tags to resource

--- 01/main.tf
+++ 01/main.tf
@@ -17,10 +17,9 @@ provider "aws"{
 # Create our first resources AWS VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  description = "My first VPC resource through Terraform"
 
-  tags {
-    Name = "VPC Name"
-    Version = "VPC Version"
+  tags = {
+    "Name" = "Name Tag"
+    "Version" = "Version Tag"
   }
 }
