b781b54417485aee90449a5c5d892eb858e628a0
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-08-29 19:17:34 +0300

    Fix path

--- 01/main.tf
+++ 01/main.tf
@@ -9,8 +9,8 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws"{
-  shared_config_files      = ["/home/root/.aws/config"]
-  shared_credentials_files = ["/home/root/.aws/credentials"]
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "default"
 }
 
