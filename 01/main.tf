81f74499cf6802aa62f496caba94aeab70535adb
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-08-29 20:04:32 +0300

    Add back remvoed s3 file

--- 01/main.tf
+++ 01/main.tf
@@ -40,3 +40,14 @@ resource "aws_s3_bucket" "myfirsts3bucket" {
   }
 }
 
+# Upload a file to S3 bucket after creation
+resource "aws_s3_object" "myfile" {
+  bucket = "terraform-aws-course-example-bucket-en"
+  key    = "myfile"
+  source = "./myfile.txt"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = file("myfile.txt")
+}
